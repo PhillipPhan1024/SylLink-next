95a1b12cd0eb4b96afbb0fb377fcbc7e6845c9e3
Phillip Phan <colPhilPhan@gmail.com>
2024-07-08 23:12:07 -0500

    Auth System works

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { SignupForm } from "./components/SignUpForm";
+
+const SignUpForm = () => {
+  return ( 
+    <div className="flex h-svh items-center"><SignupForm/></div>
+   );
+}
+
+export default SignUpForm;
